c3c0b15b10c0775ed240aed9549fd5b6551e94d3
hokula <adelhokula18@gmail.com>
2021-03-21 13:55:45 +0100

    Nesting For Loops

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,4 +1,18 @@
 function multiplyArrayFunction(myArray) {
-    var product;
-    var sum;
+    var sum = 0;
+    var product = 1;
+    var array = [];
+    for (var i = 0; i < myArray.length; i++) {
+        for (var j = 0; j < myArray[i].length; j++) {
+            sum += myArray[i][j];
+            product *= myArray[i][j];
+        }
+    }
+    array.push(product, sum);
+    return array;
 }
+
+console.log(multiplyArrayFunction( [[2], [5, 6, 7], [8, 9]]));
+console.log(multiplyArrayFunction( [[2.5, 2], [0.5, 0.2], [8]]));
+console.log(multiplyArrayFunction( [[1, 2], [3, 4, 5, 6], [7, 8, 9]]));
+module.exports = multiplyArrayFunction; 
