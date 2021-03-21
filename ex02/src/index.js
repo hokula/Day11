eef9a6d0b8948b3f8a74364258fa934e063d5cea
hokula <adelhokula18@gmail.com>
2021-03-21 12:39:02 +0100

    Multi-Dimensional Arrays

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,23 +1,26 @@
 var temp = [
-  [31, 32, 19, 37],
-  [29, 27, 55, 36],
-  [17, 27, 42, 46],
-  [29, 52, 21, 64],
-  [91, 27, 31, 61],
+    [31, 32, 19, 37],
+    [29, 27, 55, 36],
+    [17, 27, 42, 46],
+    [29, 52, 21, 64],
+    [91, 27, 31, 61]
 ];
+
 function myArrayFunction(arr) {
   var newTemps = [...arr];
-  var avarageDayTemp = [];
-
-  for (var i = 0; i < newTemps.length; i++) {
-    for (var j = 0; j < newTemps[i].length; j++) {
-      avarageDayTemp = "";
-      var total = 0;
-      total = newTemps[i][j] + newTemps[i][j] + newTemps[i][j] + newTemps[i][j];
-      avarageDayTemp = total / 4;
-      return avarageDayTemp;
+  var averageDayTemp = [];
+  var sum = 0;
+  var total = 0;
+  for (var i = 0; i < newTemps.length; ++i) {
+    for (var j = 0; j < newTemps[i].length; ++j) {
+      sum += newTemps[i][j];
     }
+      total = sum / newTemps[i].length;
+      averageDayTemp.push(total);
+      sum = 0;
+      total = 0;
   }
+  return averageDayTemp;
 }
 
 console.log(myArrayFunction(temp));
