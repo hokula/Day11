7b71cb62d4f22be2e3a2f3c48933b2b5178f1426
hokula <adelhokula18@gmail.com>
2021-03-22 00:39:15 +0100

    Splice

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -1,6 +1,13 @@
 function mySplice(arr1, arr2, n) {
-    var arr2 = arr1.slice();
-    return arr2;
+    var array2 = arr2.slice();
+    for (var i = 0; i < arr1.length; i++) {
+        array2.splice(n, 0, arr1[i]);
+    }
+    return array2;
 }
+
 console.log(mySplice([1, 2, 3], [4, 5], 1));
-module.exports = mySplice;
+console.log(mySplice([1, 2, 3], [4, 5], 2));
+console.log(mySplice(["b", "c"], ["a", "d"], 1));
+console.log(mySplice(["Dell", "Philips"], ["Samsung", "AOX", "Sony"], 2))
+module.exports = mySplice; 
