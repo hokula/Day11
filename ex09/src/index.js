1349d82b7a53d5686ec07b8280f27ab51ff56cfb
hokula <adelhokula18@gmail.com>
2021-03-21 15:22:34 +0100

    Falsy Ejector

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,12 +1,8 @@
 function myBouncer(arr) {
-    var noviArr = [];
-    for(var i = 0; i < arr.length; i++) {
-        if (arr[i] == false || arr[i] == " " || arr[i] == null || arr[i] == 0 || arr[i] == NaN || arr[i] == undefined) {
-        } else {
-            noviArr.push(arr[i]);}
-    }
-    return noviArr;
+  var noviArr = arr.filter(Boolean);
+  return noviArr;
 }
+
 console.log(myBouncer([7, "ate", "", false, 9]));
 console.log(myBouncer(["a", "b", "c"]));
 console.log(myBouncer([false, null, 0, NaN, undefined, ""]));
